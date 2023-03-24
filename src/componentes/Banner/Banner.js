326d078b85964e0a4b96384ec330becec038b49c
Noah Rissatti <noah.rissatii@gmail.com>
2023-03-24 10:22:01 -0300

    alteração

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,11 +1,10 @@
 import './Banner.css'
-import minhaImagem from "C:\\Users\\User\\organo\\src\\imagens\\Design sem nome (5).png"
 
 export const Banner = () => {
     // JSX
     return (
         <header className="banner">
-            <img src={minhaImagem} alt="O banner principal da página do Organo"/>
+            <img src="src\imagens\Design sem nome (5).png" alt="O banner principal da página do Organo"/>
         </header>
     )
 }
