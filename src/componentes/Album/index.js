bc5ae42d0ec879ccfa42225d74cd271aa54fd91b
Noah Rissatti <noah.rissatii@gmail.com>
2023-03-24 10:39:26 -0300

    alteração 2

--- src/componentes/Album/index.js
+++ src/componentes/Album/index.js
@@ -7,7 +7,7 @@ const Album = (props) => {
     return (
         (props.colaboradores.length > 0) ? <section className='album' style={css}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-            <div className='colaboradores'>
+            <div className='musicas'>
                 {props.colaboradores.map( musica => <Musica corDeFundo={props.corPrimaria} key={musica.nome} nome={musica.nome} imagem={props.imagem} duracao={musica.duracao}/> )}
             </div>
         </section> 
